de0900e2e851d711dbe69d1042691956b79ee37a
Nhi Nguyen <nhi_nguyen1610@hotmail.com>
2022-04-30 23:08:15 +1000

    data for slider in homepage

--- src/slider-data.jsx
+++ src/slider-data.jsx
@@ -9,7 +9,7 @@ const sliderData = [
   {
     img: 'https://media.cntraveler.com/photos/589b9996510ec9091d88bfa9/master/w_2580%2Cc_limit/room-service-06-Thompson-Chicago.jpg',
     heading: '5 Star Service',
-    desc: 'Beach Resort passionate team delight in treating each guest with their best services. From personalised itinerary, to thoughtful in room touches that will make your staying experience worthwhile.',
+    desc: `COAST's passionate team delight in treating each guest with their best services. From personalised itinerary, to thoughtful in room touches that will make your staying experience worthwhile.`,
   },
   {
     img: 'https://media.cntraveler.com/photos/5bb2624e625211259a973833/16:9/w_2560,c_limit/JW-Marriott-Phu-Quoc-Emerald-Bay-Resort-&-Spa__2018_GL_20161025_445.jpg',
